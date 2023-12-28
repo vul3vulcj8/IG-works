346fb02307bb9e7e207163e00e9a79a6614aeaae
Claire <29aulaul3@gmail.com>
2023-12-28 16:21:49 +0800

    finish

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+
 const jsonServerPort = Number(process.env.JSON_SERVER_PORT) || 3004;
 
 export default defineConfig({
