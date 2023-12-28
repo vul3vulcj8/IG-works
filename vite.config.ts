1106f79c81189f98ad25bca15dffecacbb677f6a
Claire <29aulaul3@gmail.com>
2023-12-28 15:09:29 +0800

    finish

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,28 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+const jsonServerPort = Number(process.env.JSON_SERVER_PORT) || 3004;
 
 export default defineConfig({
   plugins: [react()],
-  base: "IG-works",
-  // server: {
-  //   port: 3005,
-  //   proxy: {
-  //     "/home": {
-  //       target: "http://localhost:3004",
-  //       changeOrigin: true,
-  //       rewrite: (path) => path.replace(/^\/home/, ""),
-  //     },
-  //     "/following": {
-  //       target: "http://localhost:3004",
-  //       changeOrigin: true,
-  //       rewrite: (path) => path.replace(/^\/following/, "/following/IG-works"),
-  //     },
-  //     "/notFound": {
-  //       target: "http://localhost:3004",
-  //       changeOrigin: true,
-  //       rewrite: (path) => path.replace(/^\/notFound/, "/notFound/IG-works"),
-  //     },
-  //   },
-  // },
+  base: "/IG-works/",
+  server: {
+    port: jsonServerPort,
+    proxy: {
+      "/home": {
+        target: `http://localhost:${jsonServerPort}/`,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/home/, ""),
+      },
+      "/following": {
+        target: `http://localhost:${jsonServerPort}/`,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/following/, "/following/IG-works"),
+      },
+      "/notFound": {
+        target: `http://localhost:${jsonServerPort}/`,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/notFound/, "/notFound/IG-works"),
+      },
+    },
+  },
 });
